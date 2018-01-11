6c13d1dd7dd39ae8294d3387755c2d5464f5d12d
Ori Hoch <ori@uumpa.com>
2018-01-11 12:18:33 +0200

    use the new staging environment by default

--- app/_config/config.ts
+++ app/_config/config.ts
@@ -1,7 +1,7 @@
 import { OpaqueToken } from '@angular/core';
 
 
-export const REMOTE_URL = 'https://staging-dbs.bh.org.il/searchapi/search';
+export const REMOTE_URL = 'https://staging.myjewishstory.org/searchapi/search';
 export const LOCAL_URL  = 'http://localhost:5000/search';
 export const URL = REMOTE_URL;
 // export const URL = LOCAL_URL;
